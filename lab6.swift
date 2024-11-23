175dea193d0ea8919ec0642866f53678d8da2d5b
opekk <maciek09874@gmail.com>
2024-11-23 16:42:37 +0100

    zrobione zad6.1

--- lab6.swift
+++ lab6.swift
@@ -4,8 +4,49 @@
 //
 //  Created by Maciek on 22/11/2024.
 //
-
 import Foundation
 
-print("Hello, World!")
+
+func zad6_1() {
+    
+   
+    print("podaj i: ")
+    var i = Int(readLine()!)!
+    print("podaj j: ")
+    let j = Int(readLine()!)!
+    var arr = Array(repeating: Array(repeating: 0, count: j), count: i)
+    for row in 0..<i {
+        for column in 0..<j {
+            var temp = Int.random(in: -100...100)
+            arr[row][column] = temp
+        }
+    }
+    print(arr)
+    var szuakna = 0
+    print("podaj wartosc szukana: ")
+    let szukana = Int(readLine()!)!
+    var max = arr[0][0]
+    var min = arr[0][0]
+    
+    for row in 0..<i {
+        for column in 0..<j {
+            if arr[row][column] == szukana {
+                szuakna += 1
+            }
+            if arr[row][column] > max {
+                max = arr[row][column]
+                }
+            if arr[row][column] < min {
+                min = arr[row][column]
+            }
+        }
+    }
+    print("max: \(max)")
+    print("min: \(min)")
+    print("szukana: \(szuakna)")
+}
+
+func zad6_2() {
+    
+}
 
